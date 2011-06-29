2121577a19fc59792a516508c45ab8b33fd2f974
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-06-29 16:54:39 -0700

    More url nonsense.

--- app/controllers/tricorder_controller.rb
+++ app/controllers/tricorder_controller.rb
@@ -4,7 +4,7 @@ class TricorderController < ApplicationController
     if @flow_task = FlowTask.find_by_id(params[:id])
       @rdp_output = @flow_task.outputs.detect{|i| i.file_file_name =~ /.rdp.json/}
     end
-    @json_url = @rdp_output.file.try(:url) if @rdp_output
+    @json_url = "#{root_url.sub(/\/$/, '')}#{@rdp_output.file.url}" if @rdp_output
     
     if signed_in?
       @user_tasks = current_user.flow_tasks.order("id desc").paginate(:page => 1)
